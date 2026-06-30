8b18c89d0d59177d765838db1801e29632cbb624
rangeset <2432647223@qq.com>
2026-06-30 22:18:21 +0800

    chore(format): add .editorconfig and format the header information

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,8 @@
-// =============================================
+// ==============================================================
 // Copyright (c) 2026 Team ISET
 // Licensed under the MIT.
 // https://github.com/RAT-ISET/parabolic-antenna-calculater
-// =============================================
+// ==============================================================
 // Path /src/Main.cpp
 // Main of ParabolicAntennaCalculater.
 
