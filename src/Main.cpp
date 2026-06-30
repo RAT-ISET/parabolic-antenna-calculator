e6e97d171459096186d340c9a2eb42cca235b117
rangeset <2432647223@qq.com>
2026-06-30 19:52:51 +0800

    init(cmake): initialize the CMake

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+// =============================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/parabolic-antenna-calculater
+// =============================================
+// Path /src/Main.cpp
+// Main of ParabolicAntennaCalculater.
+
+int main()
+{
+    return 0;
+}
