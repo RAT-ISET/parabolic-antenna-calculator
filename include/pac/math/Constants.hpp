b48ff8ce93aace7c4ab81f44e0ea421d9007c16f
rangeset <2432647223@qq.com>
2026-07-01 18:35:46 +0800

    feat(constants): add constants definition

---
+++ include/pac/math/Constants.hpp
@@ -0,0 +1,11 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/parabolic-antenna-calculater
+// ==============================================================
+// Path /include/pac/math/Constants.hpp
+// Definition of constants in mathematics and physics.
+
+#pragma once
+
+constexpr double C = 299792458.0;
