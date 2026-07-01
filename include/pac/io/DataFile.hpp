de0bee8d2b7ec5b80485bc2331b8b191032ef777
rangeset <2432647223@qq.com>
2026-07-01 18:45:38 +0800

    feat(io): add handler for data file

---
+++ include/pac/io/DataFile.hpp
@@ -0,0 +1,10 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/parabolic-antenna-calculater
+// ==============================================================
+// Path /include/pac/io/DataFile.hpp
+// Header file of the reader and writer for data file.
+
+#pragma once
+
---
+++ src/io/DataFile.cpp
@@ -0,0 +1,9 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/parabolic-antenna-calculater
+// ==============================================================
+// Path /src/io/DataFile.cpp
+// Reader and writer for data file.
+
+#include <pac/io/DataFile.hpp>
